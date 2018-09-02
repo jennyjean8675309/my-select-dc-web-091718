a28d17ccf648b60d69aaeea5327d711b2709ee5e
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-02 18:55:19 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
   while i < collection.length
     if (yield(collection[i]) == true)
       new_collection.push(collection[i])
-      i += 1
     end
+    i += 1
   end
 end
